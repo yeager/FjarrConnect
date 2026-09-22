26a77457a8c6d85e41f488524425bd2cf2016880
Daniel Nylander <github@danielnylander.se>
2026-09-22 07:41:58 +0200

    Center About panel credits

--- App/AboutPanel.swift
+++ App/AboutPanel.swift
@@ -7,6 +7,13 @@ enum AboutPanel {
         let author = NSLocalizedString("about.author", comment: "")
         let repository = NSLocalizedString("about.repository", comment: "")
         let result = NSMutableAttributedString(string: author + "\n\n" + repository)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        result.addAttribute(
+            .paragraphStyle,
+            value: paragraphStyle,
+            range: NSRange(location: 0, length: result.length)
+        )
         let range = NSRange(location: author.utf16.count + 2, length: repository.utf16.count)
         result.addAttribute(.link, value: repositoryURL, range: range)
         return result
--- Tests/AboutPanelTests.swift
+++ Tests/AboutPanelTests.swift
@@ -8,5 +8,7 @@ final class AboutPanelTests: XCTestCase {
         let range = try XCTUnwrap(credits.string.range(of: NSLocalizedString("about.repository", comment: "")))
         let location = credits.string.distance(from: credits.string.startIndex, to: range.lowerBound)
         XCTAssertEqual(credits.attribute(.link, at: location, effectiveRange: nil) as? URL, AboutPanel.repositoryURL)
+        let paragraphStyle = try XCTUnwrap(credits.attribute(.paragraphStyle, at: 0, effectiveRange: nil) as? NSParagraphStyle)
+        XCTAssertEqual(paragraphStyle.alignment, .center)
     }
 }
